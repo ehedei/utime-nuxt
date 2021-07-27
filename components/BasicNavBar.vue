a73b020d7a42b528e28a9563dc9a840d4c33848f
ehedei <ehedeidev@gmail.com>
2021-07-27 18:07:26 +0100

    Nav bar fixed

--- components/BasicNavBar.vue
+++ components/BasicNavBar.vue
@@ -10,43 +10,11 @@
       height="60%"
       color="transparent"
     >
-      <v-menu bottom left>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon color="white" v-bind="attrs" v-on="on">
-            <v-avatar>
-              <v-icon>mdi-account-circle</v-icon>
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-card min-width="200px">
-          <v-list-item-content class="justify-center">
-            <div class="mx-auto text-center">
-              <v-avatar color="indigo darken-4 white--text my-2 elevation-3">
-                <v-img v-if="user.image" :src="user.image" :alt="user.username" cover/>
-                <v-icon v-else color="white">mdi-account</v-icon>
-              </v-avatar>
-              <h3 class="mt-1">{{ user.username }}</h3>
-              <p class="text-caption mt-1 mb-1">
-                {{ user.email }}
-              </p>
-              <p v-if="user.role !== 'user'" class="text-caption">
-                {{ user.role }}
-              </p>
-              <v-divider class="my-3"></v-divider>
-              <v-btn depressed block text to="/profile">
-                <v-icon class="me-1">mdi-account-box-outline</v-icon> Profile
-              </v-btn>
-              <v-divider class="my-3"></v-divider>
-              <v-btn depressed block text @click="logout"
-                ><v-icon class="me-1">mdi-exit-to-app</v-icon> Logout
-              </v-btn>
-            </div>
-          </v-list-item-content>
-        </v-card>
-      </v-menu>
-
-      <v-toolbar-title to="/home" class="d-flex align-center pb-1 nav-title" @click="goHome"
-        >
+      <v-toolbar-title
+        to="/home"
+        class="d-flex align-center pb-1 nav-title ms-7"
+        @click="goHome"
+      >
         <v-img
           max-height="42"
           max-width="42"
@@ -57,9 +25,37 @@
       >
 
       <v-spacer></v-spacer>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon v-if="$vuetify.breakpoint.smAndDown" @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" right app temporary light>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      :permanent="$vuetify.breakpoint.mdAndUp ? true : false"
+      :right="$vuetify.breakpoint.smAndDown ? true : false"
+      light
+    >
+      <v-card min-width="200px">
+        <v-list-item-content class="justify-center">
+          <div class="mx-auto text-center">
+            <v-avatar color="indigo darken-4 white--text my-2 elevation-3">
+              <v-img
+                v-if="user.image"
+                :src="user.image"
+                :alt="user.username"
+                cover
+              />
+              <v-icon v-else color="white">mdi-account</v-icon>
+            </v-avatar>
+            <h3 class="mt-1">{{ user.username }}</h3>
+            <p class="text-caption mt-1 mb-1">
+              {{ user.email }}
+            </p>
+            <p v-if="user.role !== 'user'" class="text-caption">
+              {{ user.role }}
+            </p>
+          </div>
+        </v-list-item-content>
+      </v-card>
       <v-list nav dense>
         <v-list-item-group active-class="indigo--text text--accent-4">
           <v-list-item to="/home">
@@ -67,6 +63,12 @@
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/profile">
+            <v-list-item-icon>
+              <v-icon>mdi-account-box</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Profile</v-list-item-title>
           </v-list-item>
           <v-list-item to="/appointments">
             <v-list-item-icon>
@@ -104,6 +106,14 @@
           </v-list-item>
         </v-list-item-group>
       </v-list>
+      <v-list-item-content class="justify-center">
+        <div class="mx-auto text-center">
+          <v-divider class="my-3"></v-divider>
+          <v-btn depressed block text @click="logout"
+            ><v-icon class="me-1">mdi-exit-to-app</v-icon> Logout
+          </v-btn>
+        </div>
+      </v-list-item-content>
     </v-navigation-drawer>
   </div>
 </template>
@@ -126,7 +136,7 @@ export default {
     },
     goHome() {
       this.$router.push('/home')
-    }
+    },
   },
 }
 </script>
@@ -146,7 +156,7 @@ export default {
 }
 
 .nav-title {
-    font-family: 'Josefin Sans', sans-serif;
-    cursor: pointer;
+  font-family: 'Josefin Sans', sans-serif;
+  cursor: pointer;
 }
 </style>
