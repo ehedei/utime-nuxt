4ebb32455d1834943985566b5b3386c9fd59c38c
juanpablo <juanpablo272394@gmail.com>
2021-07-27 09:35:36 +0100

    inject user updateProfile

--- plugins/user.service.js
+++ plugins/user.service.js
@@ -1,4 +1,5 @@
 export default ({ $axios }, inject) => {
   inject('signup', async (newUser) => await $axios.$post('/auth/signup', newUser))
   inject('getUser', async (id) => await $axios.$get(`/user/${id}`))
+  inject('updateProfile', async (userData) => await $axios.$put('/user/profile', userData))
 }
